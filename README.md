326bd7ddfbc8d753223cfde6d728212c36c44fe9
Wisser <wisserwendelin@gmail.com>
2020-04-13 14:26:33 +0200

    Add example usage; Rename class

--- README.md
+++ README.md
@@ -3,3 +3,22 @@ https://new.scoresaber.com/api/
 
 ### Compile TypeScript
 ``npx tsc``
+
+### Usage
+```typescript
+import {ScoreSaberApi} from "./client";
+import {Player, PlayerInfo, ScoreStats} from "./representations";
+
+const api: ScoreSaberApi = new ScoreSaberApi();
+
+api
+    .getPlayer('76561198098563481')
+    .then((player: Player) => {
+        const playerInfo: PlayerInfo = player.playerInfo;
+        const stats: ScoreStats = player.scoreStats;
+
+        // do something with your data
+    })
+    .catch(console.error);
+
+```
--- src/client.ts
+++ src/client.ts
@@ -1,11 +1,11 @@
 import {Player} from "./representations";
 import {IRestResponse, RestClient} from "typed-rest-client/RestClient";
 
-export class ScoreSaberPort {
+export class ScoreSaberApi {
 
     private static readonly HOST: string = 'https://new.scoresaber.com/api/player/';
 
-    private readonly restClient: RestClient = new RestClient(null, ScoreSaberPort.HOST);
+    private readonly restClient: RestClient = new RestClient(null, ScoreSaberApi.HOST);
 
     public async getPlayer(id: string): Promise<Player> {
         const response: IRestResponse<Player> = await this.restClient.get<Player>(`${id}/full/`);
